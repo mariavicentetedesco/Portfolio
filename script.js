b61c736009b98428b1976657d9185548f7f5a064
Maria VT <192327908+mariavicentetedesco@users.noreply.github.com>
2025-04-22 09:47:07 +0100

    First commit

---
+++ script.js
@@ -0,0 +1,83 @@
+//Function to make pic appear and dissapear
+
+let project1 = document.getElementById('company-home');
+let pic1 = document.getElementById('company-pic');
+
+project1.onclick = function showProject() {
+    if (pic1.style.display === 'block') {
+        pic1.style.display = 'none';
+    } else {
+    pic1.style.display = 'block';
+    }
+}
+
+
+
+let project2 = document.getElementById('dasmoto');
+let pic2 = document.getElementById('dasmoto-pic');
+
+project2.onclick = function showProject() {
+    if (pic2.style.display === 'block') {
+        pic2.style.display = 'none';
+    } else {
+    pic2.style.display = 'block';
+    }
+}
+
+
+let project3 = document.getElementById('tea-cozy');
+let pic3 = document.getElementById('tea-pic');
+
+project3.onclick = function showProject() {
+    if (pic3.style.display === 'block') {
+        pic3.style.display = 'none';
+    } else {
+    pic3.style.display = 'block';
+    }
+}
+
+
+let project4 = document.getElementById('number-guesser');
+let pic4 = document.getElementById('number-pic');
+
+project4.onclick = function showProject() {
+    if (pic4.style.display === 'block') {
+        pic4.style.display = 'none';
+    } else {
+    pic4.style.display = 'block';
+    }
+}
+
+let project5 = document.getElementById('mixed-msg');
+let pic5 = document.getElementById('mixed-pic');
+
+project5.onclick = function showProject() {
+    if (pic5.style.display === 'block') {
+        pic5.style.display = 'none';
+    } else {
+    pic5.style.display = 'block';
+    }
+}
+
+let project6 = document.getElementById('mystery-org');
+let pic6 = document.getElementById('mystery-pic');
+
+project6.onclick = function showProject() {
+    if (pic6.style.display === 'block') {
+        pic6.style.display = 'none';
+    } else {
+    pic6.style.display = 'block';
+    }
+}
+
+let project7 = document.getElementById('rock-paper');
+let pic7 = document.getElementById('rock-pic');
+
+project7.onclick = function showProject() {
+    if (pic7.style.display === 'block') {
+        pic7.style.display = 'none';
+    } else {
+    pic7.style.display = 'block';
+    }
+}
+
